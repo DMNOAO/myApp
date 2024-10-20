f860035e60f9db1a9319cf8d660084669fd1295a
DMNOAO <167371629+DMNOAO@users.noreply.github.com>
2024-10-20 02:03:26 -0300

    Login, formulario y btn

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email: string = '';
+  password: string ='';
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  onLogin() {
+    // Aquí puedes añadir la lógica de autenticación
+    if (this.email === 'usuario@ejemplo.com' && this.password === '123456') {
+      // Redirigir al usuario a la página principal si el login es correcto
+      this.navCtrl.navigateForward('/home');
+    } else {
+      // Mostrar mensaje de error (esto es solo un ejemplo)
+      alert('Credenciales incorrectas');
+    }
   }
-
+  // Función para navegar a la página Home
+  goToHome() {
+    this.navCtrl.navigateBack('/home');
+  }
 }
